0d1c6fe5a98bccd8ceb3389ed3332bfdeee7b42e
Jake <mcinteerj@gmail.com>
2020-05-19 21:36:37 +0100

    Initial commit

---
+++ generate_config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+docsToGenerate = 112
+#docsToGenerate = 100000
+maxGenerateChunkSize = 10000
+processes = 3
+reportingIntervalSeconds = 1
---
+++ read_config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+procsRatio = [2,1]
+docsRatio = [1,100000]
+readDurationSeconds = 60
+intervalResultReportingSeconds = 5
+threadsPerProc = 8
